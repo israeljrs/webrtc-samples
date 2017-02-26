48280dd59f504c064bd038ca7b5218ed8416ee61
israeljrs <israeljrs@gmail.com>
2017-02-26 11:30:01 -0300

    add code to capture webcam.

--- js/global.js
+++ js/global.js
@@ -1,6 +1,20 @@
 window.addEventListener('load', function() {
 	var btn1 = document.getElementById('btn1');
+	var video = document.getElementById('myCam');
+	var vgaConstraints = {
+	  video: {width: {exact: 640}, height: {exact: 480}}
+	};
+	function gotStream(mediaStream) {
+	  window.stream = mediaStream; // stream available to console
+	  video.srcObject = mediaStream;
+	};
 	btn1.addEventListener('click', function() {
-		alert('Hello World !!!');
+		navigator.mediaDevices.getUserMedia(vgaConstraints)
+		  	.then(gotStream)
+		  	.catch(function(e) {
+		    	var message = 'getUserMedia error: ' + e.name;
+		    	alert(message);
+		    	console.log(message);
+		});
 	});
 });
